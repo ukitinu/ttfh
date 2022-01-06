1e66cd7e4f9b2647776821eacec3b45c2e35b9c8
ukitinu <ukitinu@gmail.com>
2022-01-06 16:42:45 +0100

    Add panel picker in Window

--- src/graphics/window.py
+++ src/graphics/window.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import sys
 import tkinter as tk
+from typing import Dict, Literal
 
 from src import ini
 from src.graphics import mainpanels
@@ -15,12 +16,19 @@ class Window:
     _HEIGHT = 400
     _POS_X = 100
     _POS_Y = 100
+    _TRANS_DURATION = 3
 
     def __init__(self, clock: Clock):
         self.window = tk.Tk()
+        self.clock: Clock = clock
+        self.panels: Dict[Literal['main', 'trans'], Panel] = {
+            "main": mainpanels.create_main_panel(self.window, self.clock, self._WIDTH, self._HEIGHT),
+            "trans": mainpanels.create_transition_panel(self.window, self.clock, self._WIDTH, self._HEIGHT)
+        }
+        self.active: Literal['main', 'trans'] = "main"
+        self.trans_count: int = 0
+
         self.window.wm_protocol("WM_DELETE_WINDOW", self._on_delete)
-        self.clock = clock
-        self.panel: Panel = mainpanels.create_main_panel(self.window, self.clock, self._WIDTH, self._HEIGHT)
 
     def _on_delete(self) -> None:
         """
@@ -38,8 +46,27 @@ class Window:
         with open('continue', 'w', encoding='utf-8') as batch_file:
             batch_file.write(f'{ini.get_sys("entrypoint")} {save_time}\n')
 
+    def _pick_panel(self) -> Panel:
+        return self.panels[self.active]
+
+    def _should_switch(self) -> bool:
+        if self.active == "main":
+            is_min = self.clock.minute == self.clock.START_MINUTE
+            is_hour = self.clock.hour == self.clock.START_HOUR
+            is_day = self.clock.day != self.clock.START_DAY
+            return is_min and is_hour and is_day
+        else:
+            return self.trans_count >= self._TRANS_DURATION
+
+    def _switch(self) -> None:
+        self.active = "main" if self.active == "trans" else "trans"
+        self.trans_count = 0
+        self._pick_panel().draw()
+
     def _tick(self):
-        self.panel.tick()
+        self._pick_panel().tick()
+        if self.active == "trans":
+            self.trans_count += 1
 
     def draw(self) -> None:
         """
@@ -50,12 +77,14 @@ class Window:
         self.window.resizable(False, False)
         self.window.title('Till the Final Hour')
 
-        self.panel.draw()
+        self._pick_panel().draw()
 
         def trigger_change():
             if self.clock.running:
                 self.clock.update_time()
                 self._tick()
+            if self._should_switch():
+                self._switch()
             self.window.after(self.clock.get_interval(), trigger_change)
 
         self.window.after(self.clock.get_interval(), trigger_change)
