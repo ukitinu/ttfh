9569539bada69c01a54ffbda5da7d1d2bb43bad4
ukitinu <ukitinu@gmail.com>
2022-01-09 19:51:09 +0100

    Move SetupData class in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,19 @@
+from os.path import exists
+
 from src import ini
 
-from src.setupdata import SetupData
-from os.path import exists
+
+class SetupData:
+    def __init__(self, py_start: str, entry_start: str):
+        self.py_start = py_start
+        self.entry_start = entry_start
+
+    def get_py_line(self, py_cmd: str) -> str:
+        return f'{self.py_start}"{py_cmd}"\n'
+
+    def get_entry_line(self, entry_name: str) -> str:
+        return f'{self.entry_start}"{entry_name}"\n'
+
 
 SH_DATA: SetupData = SetupData('PYTHON_CMD=', 'ENTRYPOINT=')
 VBS_DATA: SetupData = SetupData('pythonCmd = ', 'entrypoint = ')
