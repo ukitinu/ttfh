10d7a5aa30dcdb98ae31569eb4788cc438b08c17
ukitinu <ukitinu@gmail.com>
2022-01-11 18:13:18 +0100

    Use list of Callables instead of ButtonAction in button __init__

--- src/graphics/buttons.py
+++ src/graphics/buttons.py
@@ -2,62 +2,48 @@ from __future__ import annotations
 
 import logging
 import tkinter as tk
-from typing import Callable, List, Union, Optional
+from typing import Callable, List
 
 from src.graphics.interfaces import Tickable
-from src.timer import Clock
 
 LOG = logging.getLogger(__name__)
 
 
 class ButtonAction:
-    def __init__(self,
-                 clock: Clock,
-                 start: Optional[List[Callable]] = None,
-                 middle: Optional[List[Callable]] = None,
-                 end: Optional[List[Callable]] = None):
+    def __init__(self, callables: List[Callable]):
         """
-        :param start: callables to execute first
-        :param middle: callables and switch's set_on to execute in-between. If the str is None the callable
-        is executed without argument, otherwise it is executed with getattr(self.clock, str)
-        :param end: callables to execute last
+        :param callables: ordered callables
         """
-        if start is None:
-            start = []
-        if middle is None:
-            middle = []
-        if end is None:
-            end = []
-        self.clock: Clock = clock
-        self.start: List[Callable] = start
-        self.middle: List[Callable] = middle
-        self.end: List[Callable] = end
+        self.callables: List[Callable] = callables
 
     def exec(self) -> None:
         """ Executes the button action """
-        for cmd in self.start:
-            cmd()
-        for cmd in self.middle:
-            cmd()
-        for cmd in self.end:
+        for cmd in self.callables:
             cmd()
 
 
 class Button:
     _RELIEF = tk.GROOVE
 
-    def __init__(self, root: tk.Tk, icon_path: str, action: Union[ButtonAction, Callable], **kwargs):
+    def __init__(self, root: tk.Tk, icon_path: str, action: List[Callable], **kwargs):
         """
         :param root: root Tk of the button
         :param icon_path: path to the PNG icon
-        :param action: action to call on click
+        :param action: ordered list of callables to call on click
         :param kwargs: additional arguments for the button's creation
         """
+        action.append(lambda: LOG.info('Clicked on %s', str(self)))
+
         self.root: tk.Tk = root
-        self.icon: tk.PhotoImage = tk.PhotoImage(file=icon_path)
-        self.action: Union[ButtonAction, Callable] = action
+        self.icon_path: str = icon_path
+        self.icon: tk.PhotoImage = tk.PhotoImage(file=self.icon_path)
+        self.action: ButtonAction = ButtonAction(action)
         self.style_args = kwargs
         self._btn: tk.Button = None
+
+    def __str__(self):
+        """ Uses the icon path to identify the button, override as needed. """
+        return f'button::{self.icon_path}'
 
     def place(self, pos_x: int, pos_y: int) -> Button:
         """
@@ -71,7 +57,7 @@ class Button:
 
         self._btn = tk.Button(self.root,
                               image=self.icon,
-                              command=self.action.exec if isinstance(self.action, ButtonAction) else self.action,
+                              command=self.action.exec,
                               **self.style_args)
         self._btn.place(x=pos_x, y=pos_y)
         return self
@@ -85,7 +71,7 @@ class Switch(Button, Tickable):
                  root: tk.Tk,
                  icon_off: str,
                  icon_on: str,
-                 action: Union[ButtonAction, Callable],
+                 action: List[Callable],
                  track: Callable[[], bool],
                  **kwargs):
         """
@@ -95,7 +81,7 @@ class Switch(Button, Tickable):
         :param root: root Tk of the switch
         :param icon_off: path to the PNG icon used when the button is off
         :param icon_on: path to the PNG icon used when the button is on
-        :param action: action to call on click
+        :param action: ordered list of callables to call on click
         :param track: function returning a truthy value that tracks the switch's activation state
         :param kwargs: additional arguments for the button's creation
         """
@@ -103,6 +89,10 @@ class Switch(Button, Tickable):
         self.icon_on: tk.PhotoImage = tk.PhotoImage(file=icon_on)
         self.track: Callable[[], bool] = track
         self.var: tk.BooleanVar = tk.BooleanVar(root, self.track())
+
+    def __str__(self):
+        """ Logs both id and state """
+        return f'switch::{self.icon_path}::' + ('ON' if self.var.get() else 'OFF')
 
     def place(self, pos_x: int, pos_y: int) -> Switch:
         """
